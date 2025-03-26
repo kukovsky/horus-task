f1075bc068250ec8847d750626c9e240b60411db
kukovsky <j.kukowski1998@gmail.com>
2025-03-26 19:29:43 +0100

    Update test

--- src/test/java/FileCabinetImplTest.java
+++ src/test/java/FileCabinetImplTest.java
@@ -9,8 +9,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class FileCabinetImplTest {
 
     @Test
-    void findFolderByName() {
+    void thatFolderCanBeFindCorrectlyByName() {
         // given
+        // Create a list of folders
         List<Folder> folders = new ArrayList<>();
         folders.add(new FolderImpl("Folder1", "SMALL"));
         folders.add(new FolderImpl("Folder2", "MEDIUM"));
@@ -19,18 +20,23 @@ class FileCabinetImplTest {
         folders.add(new MultiFolderImpl("MultiFolder1", "MEDIUM", List.of(new FolderImpl("Folder6", "LARGE"))));
         folders.add(new MultiFolderImpl("MultiFolder2", "LARGE", List.of(new FolderImpl("Folder8", "SMALL"))));
 
+        // Create a list of multiFolders
         List<Folder> multiFolder = new ArrayList<>();
         multiFolder.add(new MultiFolderImpl("MultiFolder1", "SMALL", folders));
 
+        // Create a FileCabinetImpl object
         FileCabinetImpl fileCabinet1 = new FileCabinetImpl(folders);
         FileCabinetImpl fileCabinet2 = new FileCabinetImpl(multiFolder);
 
         // when
+        // Find the folder by name
         Optional<Folder> folder1 = fileCabinet1.findFolderByName("Folder1");
         Optional<Folder> folder2 = fileCabinet2.findFolderByName("MultiFolder1");
 
         // then
+        // Check if the folder is present
         assertTrue(folder1.isPresent());
+        // Check if the folder name is correct
         assertEquals("Folder1", folder1.get().getName());
         assertTrue(folder2.isPresent());
         assertEquals("MultiFolder1", folder2.get().getName());
@@ -38,8 +44,9 @@ class FileCabinetImplTest {
     }
 
     @Test
-    void findFoldersBySize() {
+    void thatFolderCanBeFindBySize() {
         // given
+        // Create a list of folders
         List<Folder> folders = new ArrayList<>();
         folders.add(new FolderImpl("Folder1", "SMALL"));
         folders.add(new FolderImpl("Folder2", "MEDIUM"));
@@ -47,21 +54,26 @@ class FileCabinetImplTest {
         folders.add(new FolderImpl("Folder4", "SMALL"));
         folders.add(new MultiFolderImpl("Folder5", "MEDIUM", List.of(new FolderImpl("Folder6", "LARGE"))));
         folders.add(new MultiFolderImpl("Folder7", "LARGE", List.of(new FolderImpl("Folder8", "SMALL"))));
+
         // when
+        // Create a FileCabinetImpl object
         FileCabinetImpl fileCabinet = new FileCabinetImpl(folders);
+        // Find the folders by size
         List<Folder> findFolders = fileCabinet.findFoldersBySize("SMALL");
 
         // then
+        // Check if the size of the folders is correct
         assertEquals(3, findFolders.size());
-        System.out.println(findFolders);
+        // Check if the size name of the folders is correct
         assertEquals("SMALL", findFolders.get(0).getSize());
         assertEquals("SMALL", findFolders.get(1).getSize());
         assertEquals("SMALL", findFolders.get(2).getSize());
     }
 
     @Test
-    void count() {
+    void countAllObjectsFromStructure() {
         // given
+        // Create a list of folders
         List<Folder> folders = new ArrayList<>();
         folders.add(new FolderImpl("Folder1", "SMALL"));
         folders.add(new FolderImpl("Folder2", "MEDIUM"));
@@ -70,10 +82,13 @@ class FileCabinetImplTest {
         folders.add(new MultiFolderImpl("Folder5", "MEDIUM", List.of(new FolderImpl("Folder6", "LARGE"))));
         folders.add(new MultiFolderImpl("Folder7", "LARGE", List.of(new FolderImpl("Folder8", "SMALL"))));
         // when
+        // Create a FileCabinetImpl object
         FileCabinetImpl fileCabinet = new FileCabinetImpl(folders);
+        // Count the number of folders
         int count = fileCabinet.count();
 
         // then
+        // Check if the count is correct
         assertEquals(8, count);
 
     }
